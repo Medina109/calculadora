1e46d7f7a4c67ca2316f65b06677ec09b88aceb9
Medina109 <rabbit1999@outlook.es>
2017-05-09 14:46:10 -0500

    cacuadora-sin-terminar

--- js/java.js
+++ js/java.js
@@ -1,5 +1,28 @@
-var n=0
-function apachurrar(){
-	         n++
-	         document.getElementById("HOLA").innerHTML="Mi primer javascript conteo"+n
+var n1=0,n2=0
+function suma(){
+	         
+	         n1=document.getElementById("N1").
+	         n2=document.getElementById("N2").
+	         document.getElementById("hola").innerHTML=n1+n2
+}
+var n1=0,n2=0
+function resta() {
+
+             n1=document.getElementById("N1").
+	         n2=document.getElementById("N2").
+	         document.getElementById("hola").innerHTML=n1-n2
+}
+var n1=0, n2=0
+function multiplica(){
+
+	         n1=document.getElementById("N1").
+	         n2=document.getElementById("N2").
+	         document.getElementById("hola").innerHTML=n1*n2
+}
+var n1=0, n2=0
+function divide(){
+
+	         n1=document.getElementById("N1").
+	         n2=document.getElementById("N2").
+	         document.getElementById("hola").innerHTML=n1/n2
 }
